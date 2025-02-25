f1c89e059c7fac1996a4510c058e41fdfc518961
MinseoK1m <pushseo.kim@gmail.com>
2025-02-25 12:37:03 +0900

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import SetState from './ex020/R025_SetState';
+
+function App() {
+  return(
+    <div>
+      <h1> React Study </h1>
+      <p>HTML 화면</p>
+      <SetState />
+    </div>
+  )
+}
+
+export default App;
